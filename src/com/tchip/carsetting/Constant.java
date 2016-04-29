67b6247fcf526bd6a2b22ddd5d65958cd0775ee9
zhoumushui <zhoumushui@gmail.com>
2016-04-29 18:13:56 +0800

    [ZMS]Optimize code.

--- src/com/tchip/carsetting/Constant.java
+++ src/com/tchip/carsetting/Constant.java
@@ -1,78 +1,60 @@
 package com.tchip.carsetting;
 
 public interface Constant {
-	/**
-	 * Debug：打印Log
-	 */
+	/** Debug：打印Log */
 	public static final boolean isDebug = true;
 
-	/**
-	 * 日志Tag
-	 */
+	/** 日志Tag */
 	public static final String TAG = "ZMS";
 
-	/**
-	 * SharedPreferences名称
-	 */
+	/** SharedPreferences */
 	public static final class MySP {
-		/** 名称 **/
+		/** 名称 */
 		public static final String NAME = "CarSetting";
 
-		/** 是否开机自动录像[boolean:true] **/
+		/** 是否开机自动录像[boolean:true] */
 		public static final String STR_AUTO_RECORD = "autoRecord";
 
-		/** 停车侦测是否打开[boolean:true] **/
+		/** 停车侦测是否打开[boolean:true] */
 		public static final String STR_PARKING_ON = "parkingOn";
 
-		/** 手动设置的亮度[int] **/
+		/** 手动设置的亮度[int] */
 		public static final String STR_MANUL_LIGHT_VALUE = "manulLightValue";
 	}
 
-	/**
-	 * 广播
-	 */
+	/** 广播 */
 	public static final class Broadcast {
-		/** ACC上电 **/
+		/** ACC上电 */
 		public static final String ACC_ON = "com.tchip.ACC_ON";
 
-		/** ACC下电 **/
+		/** ACC下电 */
 		public static final String ACC_OFF = "com.tchip.ACC_OFF";
 
-		/** 进入休眠 **/
+		/** 进入休眠 */
 		public static final String SLEEP_ON = "com.tchip.SLEEP_ON";
 
-		/** 取消休眠 **/
+		/** 取消休眠 */
 		public static final String SLEEP_OFF = "com.tchip.SLEEP_OFF";
 
 	}
 
 	public static final class Setting {
 
-		/**
-		 * 最大亮度
-		 */
+		/** 最大亮度 */
 		public static final int MAX_BRIGHTNESS = 196; // 255;
 
-		/**
-		 * 默认亮度
-		 */
+		/** 默认亮度 */
 		public static final int DEFAULT_BRIGHTNESS = 180;
 
-		/**
-		 * Camera自动调节亮度是否打开
-		 */
+		/** Camera自动调节亮度是否打开 */
 		public static final boolean AUTO_BRIGHT_DEFAULT_ON = false;
 	}
 
 	public static final class GravitySensor {
-		/**
-		 * 碰撞侦测是否默认打开
-		 */
+		/** 碰撞侦测是否默认打开 */
 		public static final boolean DEFAULT_ON = true;
 
-		/**
-		 * 碰撞侦测默认灵敏度Level
-		 */
+		/** 碰撞侦测默认灵敏度Level */
 		public static final float VALUE = 9.8f;
 
 		public static final int SENSITIVE_LOW = 0;
@@ -89,22 +71,16 @@ public interface Constant {
 
 	public static final class Module {
 
-		/**
-		 * 进入MagicActivity的密码
-		 */
+		/** 进入MagicActivity的密码 */
 		public static final String MagicCode = "55555";
 
-		/**
-		 * 是否有用户中心
-		 */
+		/** 是否有用户中心 */
 		public static final boolean hasUserCenter = false;
 
-		/**
-		 * 是否有拨号短信模块
-		 */
+		/** 是否有拨号短信模块 */
 		public static final boolean hasDialer = false;
 
-		/** 停车侦测是否默认打开 **/
+		/** 停车侦测是否默认打开 */
 		public static final boolean parkDefaultOn = true;
 
 		/** 是否是公版 */
@@ -114,34 +90,20 @@ public interface Constant {
 		public static final boolean hasAPNSetting = isPublic;
 	}
 
-	/**
-	 * FM发射
-	 */
+	/** FM发射 */
 	public static final class FMTransmit {
-		/**
-		 * 系统设置：FM发射开关
-		 */
+		/** 系统设置：FM发射开关 */
 		public static final String SETTING_ENABLE = "fm_transmitter_enable";
 
-		/**
-		 * 系统设置：FM发射频率
-		 */
+		/** 系统设置：FM发射频率 */
 		public static final String SETTING_CHANNEL = "fm_transmitter_channel";
 	}
 
-	/**
-	 * 路径
-	 */
+	/** 路径 */
 	public static final class Path {
 
-		/**
-		 * 字体目录
-		 */
+		/** 字体目录 */
 		public static final String FONT = "fonts/";
 	}
 
-	/**
-	 * 设置条目点击波纹速度
-	 */
-	public static final int SETTING_ITEM_RIPPLE_SPEED = 80;
 }
